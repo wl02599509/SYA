a22f4ca7a64145c8652d43b9822fecc45c83fe4f
Mike <wl02599509@gmail.com>
2022-10-01 21:25:08 +0800

    homepage and address

---
+++ app/controllers/address_controller.rb
@@ -0,0 +1,42 @@
+class AddressController < ApplicationController
+  def index
+    
+  end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  # def create
+  #   if .save
+  #     redirect_to , notice:''
+  #   else
+  #     render :new
+  #   end
+  # end
+  
+  # def edit
+  # end
+  
+  # def update
+  #   if .update(params)
+  #     redirect_to , notice: ''
+  #   else
+  #     render :edit
+  #   end
+  # end
+  
+  # def destroy
+  #   redirect_to , notice: ''
+  # end
+  
+  # private
+  # def params
+  #   params.require().permit()
+  # end
+  
+  # def find
+  # end
+end
