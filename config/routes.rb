a737da8dbba4262be2de19a25d6e98bf7b721285
Mike <wl02599509@gmail.com>
2022-10-01 23:09:35 +0800

    root page css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: "home#homepage"
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :address
-  get '/', to: "home#homepage"
 end
