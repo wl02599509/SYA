f26255712bfd6562e99a8380071f23069e2311be
Mike <wl02599509@gmail.com>
2022-10-08 02:03:17 +0800

    微調details

--- app/javascript/controllers/link_controller.js
+++ app/javascript/controllers/link_controller.js
@@ -15,39 +15,56 @@ export default class extends Controller {
 
     const createdAt= this.element.dataset.createdAt;
     const userEmail = this.element.dataset.userEmail;
+    const shortenedUrl = this.element.dataset.shortened_url;
     const url = this.element.dataset.url;
     const clicked = this.element.dataset.clicked;
 
     const newDiv = document.createElement('div');
     const childDiv = document.createElement('div');
     newDiv.className = "bg-pink-100 my-5 p-5";
-    newDiv.id = "show-first-layer"
+    newDiv.id = "show-first-layer";
 
     const createAtDiv = document.createElement('div');
     createAtDiv.className = "py-3";
+
+    const shortenedUrlDiv = document.createElement('div');
+    const shortenedUrlHref = document.createElement('a');
+    shortenedUrlDiv.append(shortenedUrlHref)
+    shortenedUrlDiv.className = "my-6";
+    shortenedUrlHref.setAttribute('href', shortenedUrl);
+    shortenedUrlHref.className = "py-3 hover:bg-blue-100 py-3";
+
     const urlDiv = document.createElement('div');
-    urlDiv.className = "py-3";
+    const urlHref = document.createElement('a');
+    urlDiv.append(urlHref)
+    urlDiv.className = "my-6";
+    urlHref.setAttribute('href', url);
+    urlHref.className = "py-3 hover:bg-blue-100 py-3";
+
     const clickedDiv = document.createElement('div');
     clickedDiv.className = "py-3";
 
-    const createAtContent = document.createTextNode(createdAt + " by " + userEmail);
-    const urlContent = document.createTextNode(url);
-    const clickedContent = document.createTextNode(clicked);
+    const createAtContent = document.createTextNode(createdAt + " By " + userEmail);
+    const shortenUrlContent = document.createTextNode(shortenedUrl)
+    const urlContent = document.createTextNode("Destination: " + url);
+    const clickedContent = document.createTextNode("Clicked: " + clicked);
 
     createAtDiv.appendChild(createAtContent);
-    urlDiv.appendChild(urlContent);
+    shortenedUrlHref.appendChild(shortenUrlContent);
+    urlHref.appendChild(urlContent);
     clickedDiv.appendChild(clickedContent);
 
     newDiv.appendChild(childDiv);
     childDiv.append(createAtDiv);
+    childDiv.append(shortenedUrlDiv);
     childDiv.append(urlDiv);
     childDiv.append(clickedDiv);
 
-    const justNewDive = document.querySelector("#show-first-layer")
+    const justNewDive = document.querySelector("#show-first-layer");
 
     theSection.childNodes.forEach(function(elm){
       if (elm === justNewDive) {
-        justNewDive.remove()
+        justNewDive.remove();
       }
     })
     theSection.insertAdjacentElement("beforeend", newDiv);
